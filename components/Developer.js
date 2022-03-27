61c8520f8c74727c5bee6c2df986f1a37b0f2d66
KennStack01 <kibadikennedy@gmail.com>
2022-03-27 18:27:19 +0800

    built the Developer Info Section

---
+++ components/Developer.js
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import { FcGoogle } from 'react-icons/fc'
+import { FaTwitter, FaGithub } from 'react-icons/fa'
+import { IoLogoTwitter } from 'react-icons/io'
+
+export default function Developer() {
+  const [loading, setLoading] = useState(false)
+
+  return (
+    <div className="my-4 mx-auto flex-row p-4 md:my-6 md:flex">
+      <div className="mx-auto items-center justify-center rounded-lg py-5 px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-row justify-center">
+          {/* <div>
+            <img
+              src="https://avatars.githubusercontent.com/u/67477516?v=4"
+              alt="Developer Profile Picture"
+              className="rounded-full "
+            />
+          </div> */}
+          <div className="mx-3 flex flex-col justify-center">
+            <h1 className="my-1 text-center text-lg font-semibold text-gray-700">
+              hey 👋 I'm Kenn
+            </h1>
+            <p className="mb-2 text-xs text-gray-500">
+              {' '}
+              Software Developer and Indie Maker{' '}
+            </p>
+          </div>
+        </div>
+        <div className="mx-auto my-2 flex flex-col">
+          <h4 className="text-center text-xs text-gray-500">
+            Reach out to me at anytime
+          </h4>
+
+          <a
+            href="https://twitter.com/KennKibadi"
+            target="__blank"
+            className="mx-auto my-1 rounded-lg bg-white px-5 py-2 font-semibold shadow-sm outline-none hover:shadow-lg"
+            style={{ background: '#1DA1F2' }}
+          >
+            <div className="flex flex-row text-white ">
+              <FaTwitter className="my-auto text-3xl" />
+              <h3 className="my-auto mx-2 text-sm font-medium">
+                follow on twitter{' '}
+              </h3>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
--- components/banner.js
+++ components/banner.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
 import Link from 'next/link'
 import Image from 'next/image'
 // import wavingHand from '../public/wave.png'
 import { SiPython } from 'react-icons/si'
 import { BiDoorOpen } from 'react-icons/bi'
+import Developer from './Developer'
 
 const Banner = () => {
+  let [isOpen, setIsOpen] = useState(false)
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
+
   return (
-    <div className="mx-auto flex w-full flex-row justify-between rounded-md bg-turbo-blue-500 py-5 px-4 text-white md:w-2/3 md:rounded-3xl md:py-5">
+    <div className="mx-auto flex w-full flex-row justify-between rounded-md bg-turbo-blue-500 py-4 px-4 text-white md:w-2/3 md:rounded-3xl md:py-5">
       <div className="flex flex-row">
         <div className="mx-4 my-auto hidden md:block md:text-8xl">
           {' '}
@@ -32,23 +44,73 @@ const Banner = () => {
             <p className="text-xs font-light md:text-lg ">
               All the Resources you need...
             </p>
+            <p
+              onClick={openModal}
+              className="mx-auto mt-5 cursor-pointer text-xs"
+            >
+              {' '}
+              <span className="underline"> Indie Maker</span>🛠
+            </p>
           </div>
         </div>
       </div>
       <Link href="/adsBanner">
         <a className="flex cursor-pointer flex-col place-content-end rounded-3xl bg-turbo-blue-600 py-4 px-4 md:py-10 md:px-8">
-          <p className="md:text-md text-xs md:italic">Reserved Space</p>
+          <p className="md:text-md text-xs md:italic">Sponsor Space</p>
           <h2 className="md:text-md mb-4 text-center text-xs font-bold">
-            Contact us
+            Call Kenn😎
           </h2>
           <div className="flex flex-row justify-center rounded-xl bg-yellow-500 p-2 font-bold text-turbo-blue-700">
-            <p>Open</p>
+            <p className="md:text-md text-sm">Open</p>
             <div className="my-auto text-xl">
               <BiDoorOpen />
             </div>
           </div>
         </a>
       </Link>
+
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 z-50 overflow-y-auto"
+          onClose={closeModal}
+        >
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 backdrop-blur-sm backdrop-filter" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="inline-block h-screen align-middle"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="z-50 my-8 inline-block w-full max-w-md transform overflow-hidden rounded-2xl bg-gray-50 text-left align-middle shadow-xl transition-all">
+                <Developer />
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
     </div>
   )
 }
